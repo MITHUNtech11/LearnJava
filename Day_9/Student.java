8d5afaf0d7aba46f7e0724538a207f2056060be5
Mithun <mithunsenthils4056.sse@saveetha.com>
2026-03-23 14:10:31 +0530

    leaning class and objects

---
+++ Day_9/Student.java
@@ -0,0 +1,12 @@
+package Day_9;
+class Student{
+    int id;
+    String name;
+    public static void main(String[] args){
+        Student s=new Student();
+        s.id=101;
+        s.name="Mithun";
+        System.out.println(s.id);
+        System.out.println(s.name);   
+    }
+}
---
+++ Day_9/n.java
@@ -0,0 +1,5 @@
+package Day_9;
+
+public class n {
+    
+}
